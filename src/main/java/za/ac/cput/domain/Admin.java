aa75e5d2b0f25ec8f91549cf7307e20813b1c9f6
TylorTazz2024 <165163298+TylorTazz2024@users.noreply.github.com>
2025-05-18 23:20:38 +0200

    Test

--- src/main/java/za/ac/cput/domain/Admin.java
+++ src/main/java/za/ac/cput/domain/Admin.java
@@ -1,4 +1,93 @@
 package za.ac.cput.domain;
 
+import java.util.Objects;
+/* Employee.java
+
+     AdminFactory POJO class
+
+     Author: T.Malifethe (222602511)
+
+     Date: 11 May 2025 */
+
 public class Admin {
+    private Long adminID;
+    private String staffNumber;
+    private String department;
+
+    public Admin() {
+    }
+
+    public Admin(AdminBuilder builder) {
+        this.adminID = builder.adminID;
+        this.staffNumber = builder.staffNumber;
+        this.department = builder.department;
+    }
+
+    public Long getAdminID() {
+        return adminID;
+    }
+
+    public String getStaffNumber() {
+        return staffNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Admin)) return false;
+        Admin admin = (Admin) o;
+        return Objects.equals(adminID, admin.adminID) &&
+                Objects.equals(staffNumber, admin.staffNumber) &&
+                Objects.equals(department, admin.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(adminID, staffNumber, department);
+    }
+
+    @Override
+    public String toString() {
+        return "AdminFactory{" +
+                "adminID=" + adminID +
+                ", staffNumber='" + staffNumber + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    public static class AdminBuilder {
+        private Long adminID;
+        private String staffNumber;
+        private String department;
+
+        public AdminBuilder setAdminID(Long adminID) {
+            this.adminID = adminID;
+            return this;
+        }
+
+        public AdminBuilder setStaffNumber(String staffNumber) {
+            this.staffNumber = staffNumber;
+            return this;
+        }
+
+        public AdminBuilder setDepartment(String department) {
+            this.department = department;
+            return this;
+        }
+
+        public AdminBuilder copy(Admin admin) {
+            this.adminID = admin.adminID;
+            this.staffNumber = admin.staffNumber;
+            this.department = admin.department;
+            return this;
+        }
+
+        public Admin build() {
+            return new Admin(this);
+        }
+    }
 }
--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -1,4 +1,105 @@
 package za.ac.cput.domain;
+/* Employee.java
+
+     AdminFactory POJO class
+
+     Author: T.Malifethe (222602511)
+
+     Date: 11 May 2025 */
 
 public class User {
+    private int userID;
+    private String firstname;
+    private String email;
+    private String phone;
+    private String role;
+
+
+    private User(Builder builder) {
+        this.userID = builder.userID;
+        this.firstname = builder.firstname;
+        this.email = builder.email;
+        this.phone = builder.phone;
+        this.role = builder.role;
+    }
+
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getfirstname() {
+        return firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+
+    public static class Builder {
+        public String firstname;
+        private int userID;
+        private String email;
+        private String phone;
+        private String role;
+
+        public Builder setUserID(int userID) {
+            this.userID = userID;
+            return this;
+        }
+
+        public Builder setfirstname(String firstname) {
+            this.firstname = firstname;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setRole(String role) {
+            this.role = role;
+            return this;
+        }
+
+
+        public Builder copy(User user) {
+            this.userID = user.getUserID();
+            this.firstname = user.getfirstname();
+            this.email = user.getEmail();
+            this.phone = user.getPhone();
+            this.role = user.getRole();
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+
+        @Override
+        public String toString() {
+            return "User.Builder{" +
+                    "userID=" + userID +
+                    ", firstname='" + firstname+ '\'' +
+                    ", email='" + email + '\'' +
+                    ", phone='" + phone + '\'' +
+                    ", role='" + role + '\'' +
+                    '}';
+        }
+    }
 }
