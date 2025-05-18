cd881412238f6078f9881db9fb71bb269840cf89
TylorTazz2024 <165163298+TylorTazz2024@users.noreply.github.com>
2025-05-18 22:39:46 +0200

    Test

---
+++ src/test/java/za/ac/cput/factory/AdminFactoryTest.java
@@ -0,0 +1,17 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
