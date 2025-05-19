91d9af8ca01a93c1e64b92810bd299d18036ba90
Siphelele Malotana <221800662@mycput.ac.za>
2025-05-19 03:08:09 +0200

    ReviewFactory class

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,4 +1,14 @@
 package za.ac.cput.util;
 
+/*
+ * Helper.java
+ * Utility class for validation
+ * Author: S.Malotana (221800662)
+ * Date: 18 May 2025
+ */
 public class Helper {
+
+    public static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
 }
