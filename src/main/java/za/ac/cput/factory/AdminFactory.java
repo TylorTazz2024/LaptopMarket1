30274ac119b6f99af932e18d90e45c9e685af682
TylorTazz2024 <165163298+TylorTazz2024@users.noreply.github.com>
2025-05-19 15:06:08 +0200

    updated

--- src/main/java/za/ac/cput/factory/AdminFactory.java
+++ src/main/java/za/ac/cput/factory/AdminFactory.java
@@ -1,4 +1,24 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Admin;
+import za.ac.cput.util.Helper;
+
 public class AdminFactory {
+
+    public static Admin createAdmin(Long adminID,
+                                    String staffNumber,
+                                    String department) {
+
+        if (adminID == null
+                || Helper.isNullOrEmpty(staffNumber)
+                || Helper.isNullOrEmpty(department)) {
+            return null;
+        }
+
+        return new Admin.AdminBuilder()
+                .setAdminID(adminID)
+                .setStaffNumber(staffNumber)
+                .setDepartment(department)
+                .build();
+    }
 }
--- src/main/java/za/ac/cput/factory/UserFactory.java
+++ src/main/java/za/ac/cput/factory/UserFactory.java
@@ -1,4 +1,31 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.User;
+import za.ac.cput.util.Helper;
+
 public class UserFactory {
+
+    public static User createUser(int userID,
+                                  String firstname,
+                                  String email,
+                                  String phone,
+                                  String role) {
+
+        if (!Helper.isValidUserID(userID)
+                || Helper.isNullOrEmpty(firstname)
+                || !Helper.isValidEmail(email)
+                || !Helper.isValidPhone(phone)
+                || !Helper.isValidRole(role)) {
+            return null;
+        }
+
+        return new User.Builder()
+                .setUserID(userID)
+                .setfirstname(firstname)
+                .setEmail(email)
+                .setPhone(phone)
+                .setRole(role)
+                .build();
+    }
+
 }
