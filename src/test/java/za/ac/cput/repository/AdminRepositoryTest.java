90ca30c5fe319eb8b4493cc1f80ae5a8ecb745dd
TylorTazz2024 <165163298+TylorTazz2024@users.noreply.github.com>
2025-05-25 21:08:11 +0200

    Repository

---
+++ src/test/java/za/ac/cput/repository/AdminRepositoryTest.java
@@ -0,0 +1,17 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
